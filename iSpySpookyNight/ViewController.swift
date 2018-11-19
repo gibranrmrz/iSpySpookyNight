b9862eb34290767567d3a227027d092d5e432ddf
Period Three <periodthree@imac-01.local>
2018-11-19 11:41:14 -0500

    Initial Commit

---
+++ iSpySpookyNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iSpySpookyNight
+//
+//  Created by Period Three on 2018-11-19.
+//  Copyright © 2018 Period Three. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
